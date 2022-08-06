c1d45a3b8d5d1280d87e46f5550f45dbe345fefe
Lijukay <lico.keins@gmail.com>
2022-08-06 17:24:06 +0200

    Adding, changing
    I added changed the navigationbar. It now follows the color.
    
    I added a button (update) which will show off, wenn i made an update. It will send you to the release-page of this app

--- app/src/main/java/com/lijukay/quotesAltDesign/About.java
+++ app/src/main/java/com/lijukay/quotesAltDesign/About.java
@@ -6,11 +6,28 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class About extends AppCompatActivity {
+
+    int versionC;
+    int versionA;
+    private RequestQueue mRequestQueueU;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,50 @@ public class About extends AppCompatActivity {
             startActivity(intentG);
 
         });
+        mRequestQueueU = Volley.newRequestQueue(this);
+        parseJSONVersion();
+    }
+
+    private void parseJSONVersion() {
+        String urlU = "https://lijukay.github.io/quotesaltdesign/editorschoice.json";
+
+        JsonObjectRequest requestU = new JsonObjectRequest(Request.Method.GET, urlU, null,
+                responseU -> {
+                    try {
+                        JSONArray jsonArrayAll = responseU.getJSONArray("Version");
+
+                        for(int a = 0; a < jsonArrayAll.length(); a++){
+                            JSONObject v = jsonArrayAll.getJSONObject(a);
+
+                            versionC = BuildConfig.VERSION_CODE;
+                            versionA = v.getInt("VersionA");
+
+                        }
+
+                        if (versionA > versionC){
+                            Button update = findViewById(R.id.updateB);
+                            TextView updateA = findViewById(R.id.updateT);
+
+                            update.setVisibility(View.VISIBLE);
+                            updateA.setVisibility(View.VISIBLE);
+
+                            update.setOnClickListener(view -> {
+                                Uri uriU = Uri.parse("https://github.com/Lijukay/quotesaltdesign/releases");
+                                Intent intentU = new Intent(Intent.ACTION_VIEW, uriU);
+                                startActivity(intentU);
+                            });
+                        }
+
+                        Log.e("intent", "Hat geklapptAll...");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e("error", "hat nicht geklapptall...");
+                    }
+                }, errorAll -> {
+            errorAll.printStackTrace();
+            Log.e("error", "Hat nicht geklappt 2all");
+        });
+        mRequestQueueU.add(requestU);
     }
 
     @Override
